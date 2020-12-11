8e10b4bb22f1fc08684671bb024147317a8d520e
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2020-12-11 17:13:43 +0300

    главная 7h

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import "./import/modules";
 import "./import/components";
 import svgPolyfill from "../../node_modules/svg4everybody/dist/svg4everybody.js";
+svgPolyfill();
+
+import $ from 'jquery';
+import './import/list.js';
+
+$(document).ready(function() {
+    $('[data-list="collapse"]').abixTreeList({
+        collapsedIconClass: 'icon-cross',
+        expandedIconClass: 'icon-cross open',
+    });
+});
